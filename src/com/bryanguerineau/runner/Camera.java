17dd80a6bb2bdef4bd33aec776356fe362569709
Bryan Guerineau <bryanguerineau@ordinateur-portable-de-bryan.home>
2022-11-10 14:31:51 +0100

    Initial commit

---
+++ src/com/bryanguerineau/runner/Camera.java
@@ -0,0 +1,27 @@
+package com.bryanguerineau.runner;
+
+public class Camera {
+    int x;
+    int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Camera(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Camera{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}

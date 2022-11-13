7cd863403c1fb2a1b87ba43ea72fc372bd78529f
bryanguerineau <bryan.guerineau@outlook.com>
2022-11-13 19:38:38 +0100

    Création de la class abstraite AnimatedThing.java

---
+++ src/com/bryanguerineau/runner/AnimatedThing.java
@@ -0,0 +1,13 @@
+package com.bryanguerineau.runner;
+
+import javafx.scene.image.ImageView;
+
+public abstract class AnimatedThing {
+    double x;
+    double y;
+    ImageView spriteSheet;
+    int mode;
+
+
+
+}

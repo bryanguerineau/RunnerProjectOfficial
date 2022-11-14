173e3ef7f0f87ee79db394003d28e544fdce45d1
bryanguerineau <bryan.guerineau@outlook.com>
2022-11-15 00:00:04 +0100

    creation update method in GameScene.java. Personnage defile sans etre animé pour le moment

--- src/com/bryanguerineau/runner/GameScene.java
+++ src/com/bryanguerineau/runner/GameScene.java
@@ -1,5 +1,6 @@
 package com.bryanguerineau.runner;
 
+import javafx.animation.AnimationTimer;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
@@ -19,6 +20,32 @@ public class GameScene extends Scene {
         this.backgroundRight = new staticThings(0,0,"file:img/desert.png");
         this.heroCharacter = new Hero();
 
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                heroCharacter.update(l);
+                GameScene.update(l);
+
+            }
+        };
+        timer.start();
+    }
+
+    private static void update(long l) {
+        int speed = 7;
+        double xl = backgroundLeft.getSizeX();
+        double xr = backgroundRight.getSizeX();
+        if (xl < 4){
+            backgroundLeft.getImageView().setX(800);
+            backgroundLeft.setSizeX(800);
+            backgroundRight.getImageView().setX(0);
+            backgroundRight.setSizeX(0);
+        } else {
+            backgroundLeft.getImageView().setX(xl-speed);
+            backgroundLeft.setSizeX((xl-speed));
+            backgroundRight.getImageView().setX((xr-speed));
+            backgroundRight.setSizeX((xr-speed));
+        }
     }
 
     public static staticThings getBackgroundLeft() {

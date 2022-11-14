9c1f6711aa484b3aba3e1b87e193ac08382cd6a9
bryanguerineau <bryan.guerineau@outlook.com>
2022-11-14 22:59:07 +0100

    Constructor, getter for Hero

--- src/com/bryanguerineau/runner/AnimatedThing.java
+++ src/com/bryanguerineau/runner/AnimatedThing.java
@@ -1,13 +1,36 @@
 package com.bryanguerineau.runner;
 
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public abstract class AnimatedThing {
-    double x;
-    double y;
-    ImageView spriteSheet;
-    int mode;
+    private double xHero;
+    private double yHero;
+    public ImageView spriteSheet;
+    private int attitude;
 
+    private int runIndex = 0;
+    private double durationBetweenTwoFrames = 15;
+    private int maxIndex = 10;
+    private int sizeLength;
+    private int sizeHeigth;
+    private int offsetBetweenEachFrame = 10;
+    public ImageView spriteSheetHero;
 
+    public AnimatedThing(double xHero, double yHero, String fileName) {
+        Image spriteSheetHeroImage = new Image("file:img/heros.png");
+        this.spriteSheetHero = new ImageView(spriteSheetHeroImage);
+        this.spriteSheetHero.setX(xHero);
+        this.spriteSheetHero.setY(yHero);
+        this.spriteSheetHero.setViewport(new Rectangle2D(0,0,75,100));
+        this.yHero = yHero;
+        this.xHero = xHero;
+    }
 
+    public ImageView getSpriteSheet() { return spriteSheet; }
+
+    public double getxHero() { return xHero; }
+    public double getyHero() { return yHero; }
+    public int getRunIndex() { return runIndex; }
 }

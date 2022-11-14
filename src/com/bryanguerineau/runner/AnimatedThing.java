ade5fe003e8d9f67f0a0f06c67b995132ca30e68
bryanguerineau <bryan.guerineau@outlook.com>
2022-11-14 23:51:39 +0100

    creation update method in AnimatedThing.java

--- src/com/bryanguerineau/runner/AnimatedThing.java
+++ src/com/bryanguerineau/runner/AnimatedThing.java
@@ -11,15 +11,18 @@ public abstract class AnimatedThing {
     private int attitude;
 
     private int runIndex = 0;
+    private int jumpIndex = 0;
     private double durationBetweenTwoFrames = 15;
-    private int maxIndex = 10;
+    private int maxIndex = 5;
+    private int maxJump = 2;
     private int sizeLength;
     private int sizeHeigth;
     private int offsetBetweenEachFrame = 10;
     public ImageView spriteSheetHero;
+    private int jump;
 
     public AnimatedThing(double xHero, double yHero, String fileName) {
-        Image spriteSheetHeroImage = new Image("file:E:\\Devs\\Runner\\img\\heros.png");
+        Image spriteSheetHeroImage = new Image("file:img/heros.png");
         this.spriteSheetHero = new ImageView(spriteSheetHeroImage);
         this.spriteSheetHero.setX(xHero);
         this.spriteSheetHero.setY(yHero);
@@ -37,11 +40,37 @@ public abstract class AnimatedThing {
 
     public void setRunIndex(int runIndex) { this.runIndex = runIndex; }
 
+    public void setJumpIndex(int jumpIndex) { this.jumpIndex = jumpIndex; }
 
     public double getxHero() { return xHero; }
     public double getyHero() { return yHero; }
     public int getRunIndex() { return runIndex; }
 
+    public int getJumpIndex() { return jumpIndex; }
+
     public abstract ImageView getSpriteSheet();
 
+    public void update(long l) {
+        int newIndex;
+        if (jump == 0){
+            newIndex = getRunIndex();
+            if (newIndex == maxIndex) {
+                setRunIndex(0);
+            } else {
+                setRunIndex(newIndex++);
+            }
+            xHero = getxHero();
+            spriteSheetHero.setViewport(new Rectangle2D(getRunIndex()*85,0,85,100));
+        } else {
+            newIndex = getJumpIndex();
+            if (newIndex == maxIndex){
+                setJumpIndex(0);
+                jump = 0;
+            } else {
+                xHero = getxHero();
+                spriteSheetHero.setViewport(new Rectangle2D(getJumpIndex()*85,160,85,100));
+                setJumpIndex(newIndex++);
+            }
+        }
+    }
 }
